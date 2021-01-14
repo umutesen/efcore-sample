50c49bd185f6c7a2542dcf4c25fdaf67dbbd40b9
Umut Esen <umutesen@me.com>
2021-01-14 15:01:42 +0000

    First commit

---
+++ EFCoreSample/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreSample.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ EFCoreSample/Data/Book.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace EFCoreSample.Data
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
